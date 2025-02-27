4bcf9cf7db0fd3de7cb2b2fe2f47acbee1e507e4
Alice Tweedy McGrath <atmcgrath@gmail.com>
2025-02-27 18:04:52 -0500

    updates to style, add lady susan etc

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from flask import Flask
 from markupsafe import escape
 from flask import render_template
 import requests
+import csv
 #import random
 #from lxml import html, etree
 
@@ -10,7 +11,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return render_template('default.html')
+    return render_template('home.html')
 # def index():
 #     return "<html><h1>Congratulations, it's a web app!</h1><p>To use this app, enter a farenheit number.</p></html>"
 
@@ -24,10 +25,19 @@ def get_iss_people():
         return render_template('astros.html', names=astro_names)
     else:
         return 'Not found'
-# @app.route('/jane/')
-# def get_jane_austen():
-#     response = requests.get("https://www.gutenberg.org/cache/epub/946/pg946-images.html")
-#     tree = html.fromstring(response.content)
+@app.route('/jane/')
+#@app.route('/jane/<num>')
+def lady_susan():
+    with open('data/lady-susan.csv', 'r') as f:
+        csv_reader = csv.reader(f)
+        list_of_csv = list(csv_reader)
+        text = '<h2>Lady Susan</h2>\n<ul>'
+        for line in list_of_csv[1:]:
+            l = '<li>'+ line[0] + '. ' + line[2] + '</li>\n'
+            text+= l
+        text += '</ul>'
+        return render_template('misc.html', text=text)
+            
 
 #     text = response.text
 #     paragraphs = text.split('/r/n')
@@ -35,9 +45,6 @@ def get_iss_people():
 #     selection = paragraphs[r]
 #     return render_template('misc.html', text=selection)
 #     #return selection
-
-
-
 
 @app.route('/hello/')
 @app.route("/hello/<name>")
